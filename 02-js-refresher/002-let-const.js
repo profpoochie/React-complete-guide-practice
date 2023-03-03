cd10c29cee531dd499398519323c4d4da397b3f5
Marvin Borhillo <40102065+profpoochie@users.noreply.github.com>
2023-03-03 15:56:15 +0800

    Practice let and const

---
+++ 02-js-refresher/002-let-const.js
@@ -0,0 +1,18 @@
+/*var myName = 'Max';
+console.log(myName);
+
+myName = 'Manu';
+console.log(myName);*/
+
+/*let myName = 'Max';
+console.log(myName);
+
+myName = 'Manu';
+console.log(myName);*/
+
+// Error
+const myName = 'Max';
+console.log(myName);
+
+myName = 'Manu';
+console.log(myName);
