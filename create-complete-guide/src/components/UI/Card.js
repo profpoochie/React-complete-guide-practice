6845f50fee150424d2e6a570fa396ce7ca5a529a
Marvin Borhillo <40102065+profpoochie@users.noreply.github.com>
2023-03-05 22:47:29 +0800

    alternative function syntax

--- create-complete-guide/src/components/UI/Card.js
+++ create-complete-guide/src/components/UI/Card.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./Card.css";
 
-function Card(props) {
+const Card = (props) => {
   // don't forget the whitespace on the "card"
   const classes = "card " + props.className;
   return <div className={classes}>{props.children}</div>;
-}
+};
 
 export default Card;
